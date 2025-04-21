8ae2b85d603dfced89ce027d354d90dff78edd9e
Guddu Shakar Paul <guddu.paul@dotsquares.com>
2025-04-21 13:05:08 +0530

    database connect successfully

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,18 @@
 import dotenv from "dotenv";
 import { app } from "./app.js";
+import { connectDB } from "./db/index.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+    console.log("Database connected successfully");
+  })
+  .catch((error) => {
+    console.error("Database connection error:", error);
+  });
